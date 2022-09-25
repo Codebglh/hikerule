ccf283ca343d1aa5c62c37129df6827b8f6b6233
bgcode <447701537@qq.com>
2022-09-26 00:35:17 +0800

    ss

---
+++ l/moban.js
@@ -0,0 +1,9 @@
+var version = {
+    author: "bgcode",
+    ver: "0.0.1",
+    requireId: "http://hiker.nokia.press/hikerule/rulelist.json?id=2505",
+    update: '2022/08/24 14:25',
+    info: updateLog,
+    ok: 'https://okjx.cc/?url=',
+    jsRoot: 'https://gitcode.net/qq_32394351/dr/-/raw/master/js/',
+};
