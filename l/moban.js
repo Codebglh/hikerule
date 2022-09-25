5ab5a594667f1fd27ffad91c062ad95184b6acf0
bgcode <447701537@qq.com>
2022-09-26 00:37:40 +0800

    ff

--- l/moban.js
+++ l/moban.js
@@ -1,7 +1,7 @@
 var version = {
     author: "bgcode",
     ver: "0.0.1",
-    requireId: "http://hiker.nokia.press/hikerule/rulelist.json?id=2505",
+    requireId: "https://ghproxy.com/https://raw.githubusercontent.com/Codebglh/hikerule/main/l/moban.js",
     update: '2022/08/24 14:25',
     info: updateLog,
     ok: 'https://okjx.cc/?url=',
