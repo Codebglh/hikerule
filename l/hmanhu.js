d13f6aaa5687fbb79909aec00d1cf0f8c33bbcf1
bgcode <447701537@qq.com>
2022-09-28 22:18:56 +0800

    dd

---
+++ l/hmanhu.js
@@ -0,0 +1,116 @@
+var updateLog = '2022/09/26 初制作模版';
+var version = {
+    author: "bgcode",
+    ver: "0.0.1",
+    Id: "https://ghproxy.com/https://raw.githubusercontent.com/Codebglh/hikerule/main/l/moban.js",
+    update: '2022/09/26 14:25',
+    info: updateLog,
+    ok: 'https://okjx.cc/?url=',
+    url: 'https://ghproxy.com/https://raw.githubusercontent.com/Codebglh/hikerule/main/l/',
+};
+
+function yiji() {
+    var d = [];
+    if (MY_PAGE == 1) { //第一页的筛选
+        d.push({
+            title: "收藏",
+            url: "hiker://collection",
+            pic_url: version.url + 'src/2.png',
+            col_type: 'icon_4',
+        });
+        d.push({
+            title: "历史",
+            url: "hiker://history",
+            pic_url: version.url + 'src/3.png',
+            col_type: 'icon_4',
+        });
+        d.push({
+            title: "设置",
+            url: $("hiker://empty#noRecordHistory##noHistory#").rule(() => {
+                require(config.依赖);
+                shezhi();
+            }),
+            pic_url: version.url + 'src/4.png',
+            col_type: 'icon_4',
+        });
+        d.push({
+            title: "搜索",
+            url: $("hiker://empty#noRecordHistory##noHistory#").rule(() => {
+                require(config.依赖);
+                sousuo();
+            }),
+            pic_url: version.url + 'src/5.png',
+            col_type: 'icon_4',
+        });
+
+        d.push({
+            col_type: 'line'
+        });
+
+    }
+    // var url = 'https://www.onetgb.xyz'
+    var MY_URL = " https://www.mhww.xyz/?page.currentPage=" + MY_PAGE + "&orderType=3&subjectName=%E9%9F%93%E6%BC%AB&filmName=";
+    var html = fetch(MY_URL);
+    var BT = xpathArray(html, 标题);
+    var LJ = xpathArray(html, 链接);
+    var XQ = xpathArray(html, 详情);
+    var TP = xpathArray(html, 图片);
+
+    // log(TP)
+    for (var i in BT) {
+        d.push({
+            title: BT[i],
+            desc: XQ[i].replace("\r\n", ""),
+            pic_url: TP[i],
+            url: url + LJ[i] + "#immersiveTheme#",
+            col_type: 'text_2',
+        });
+
+    }
+
+
+
+
+    setResult(d);
+};
+
+function zhuye() {
+    var d = [];
+    d.push({
+        title: '<span style="color:#ff6600"><b>\t热搜榜\t\t\t</b></span>',
+        url: "hiker://empty",
+        pic_url: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=3779990328,1416553241&fm=179&app=35&f=PNG?w=60&h=70&s=E7951B62A4639D153293A4E90300401B',
+        col_type: 'icon_small_3'
+    });
+    setResult(d);
+};
+
+
+function shezhi() {
+    addListener("onClose", $.toString(() => {
+        clearMyVar('shezhi$input');
+    }));
+    var d = [];
+    d.push({
+        title: '<span style="color:#ff6600"><b>\t热搜榜\t\t\t</b></span>',
+        url: "hiker://empty",
+        pic_url: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=3779990328,1416553241&fm=179&app=35&f=PNG?w=60&h=70&s=E7951B62A4639D153293A4E90300401B',
+        col_type: 'icon_small_3'
+    });
+    setResult(d);
+};
+
+
+function sousuo() {
+    addListener("onClose", $.toString(() => {
+        clearMyVar('sousuo$input');
+    }));
+    var d = [];
+    d.push({
+        title: '<span style="color:#ff6600"><b>\t热搜榜\t\t\t</b></span>',
+        url: "hiker://empty",
+        pic_url: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=3779990328,1416553241&fm=179&app=35&f=PNG?w=60&h=70&s=E7951B62A4639D153293A4E90300401B',
+        col_type: 'icon_small_3'
+    });
+    setResult(d);
+};
