4a7463ffb3704f211c0e8831ec5f8a9e35300a82
bgcode <bglhcode@icloud.com>
2023-02-06 23:43:16 +0800

    bgcode

--- l/HMH.js
+++ l/HMH.js
@@ -97,6 +97,21 @@ function erji() {
     setResult(d);
 }
 
+function sanji() {
+    var html = getResCode()
+    var d = [];
+    var xx = xpathArray(html, "//*[@id=\"imgList\"]/img/@src");
+    var BT = xpathArray(html, "//*[@id=\"imgList\"]/img/@data-original");
+    var ll = "pics://"
+    for (var i = 1; i < xx.length; i++) {
+        ll = ll + xx[i]
+    }
+    for (var i = 0; i < BT.length; i++) {
+        ll = ll + BT[i]
+    }
+    return ll
+}
+
 function zhuye() {
     var d = [];
     d.push({
