95b78f7c4c5e4b39df663bc99da4e21eeda83862
bgcode <bglhcode@icloud.com>
2023-02-07 00:31:31 +0800

    bgcode

--- l/BGMH.js
+++ l/BGMH.js
@@ -102,7 +102,6 @@ function erji() {
 
 function sanji(url) {
     var html = fetch(url)
-    var d = [];
     var xx = xpathArray(html, "//*[@id=\"imgList\"]/img/@src");
     var BT = xpathArray(html, "//*[@id=\"imgList\"]/img/@data-original");
     var ll = "pics://"
@@ -117,7 +116,9 @@ function sanji(url) {
             ll = ll + BT[i] + '&&'
         }
     }
+    log(ll)
     return ll
+
 }
 
 function zhuye() {
