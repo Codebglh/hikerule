88e0a37c2e6bbfe2f6346ff0ff70a1da2607189d
bgcode <bglhcode@icloud.com>
2023-02-07 17:47:49 +0800

    bgcode

--- 学习/require.js
+++ 学习/require.js
@@ -1,25 +1,28 @@
-let file1 = "https://gitcode.net/src48597962/hk/-/raw/master/SrcJuying.js";
-let file2 = "https://gitea.com/src48597962/hk/raw/branch/master/SrcJuying.js";
-let file3 = "https://codeberg.org/src48597962/hk/raw/branch/master/SrcJuying.js";
+let file1 = "https://cdn.jsdelivr.net/gh/Codebglh/hikerule/main/l/BGMH.js";
+let file2 = "https://github.jevons.vip/https://raw.githubusercontent.com/Codebglh/hikerule/main/l/BGMH.js";
+let file3 = "https://cdn.staticaly.com/gh/Codebglh/hikerule/main/l/BGMH.js";
+let file4 = "https://ghproxy.com/https://raw.githubusercontent.com/"
 let relyfile = "";
-if(fileExist('hiker://files/libs/' + md5(file1) + '.js')){
-  relyfile = file1;
-}else if(fileExist('hiker://files/libs/' + md5(file2) + '.js')){
-  relyfile = file2;
-}else if(fileExist('hiker://files/libs/' + md5(file3) + '.js')){
-  relyfile = file2;
-}
-if(relyfile==""){
-  let cjFile = request(file1,{timeout:3000});
-  if(cjFile.indexOf('nowVersion') > -1){
+if (fileExist('hiker://files/libs/' + md5(file1) + '.js')) {
     relyfile = file1;
-  }else{
-    let cjFile = request(file2,{timeout:3000});
-    if(cjFile.indexOf('nowVersion') > -1){
-      relyfile = file2;
-    }else{
-      relyfile = file3;
+} else if (fileExist('hiker://files/libs/' + md5(file2) + '.js')) {
+    relyfile = file2;
+} else if (fileExist('hiker://files/libs/' + md5(file3) + '.js')) {
+    relyfile = file3;
+} else if (fileExist('hiker://files/libs/' + md5(file4) + '.js')) {
+    relyfile = file4;
+}
+if (relyfile == "") {
+    let cjFile = request(file1, {timeout: 3000});
+    if (cjFile.indexOf('nowVersion') > -1) {
+        relyfile = file1;
+    } else {
+        let cjFile = request(file2, {timeout: 3000});
+        if (cjFile.indexOf('nowVersion') > -1) {
+            relyfile = file2;
+        } else {
+            relyfile = file3;
+        }
     }
-  }
 }
   
--- 学习/url.js
+++ 学习/url.js
@@ -1,21 +1,19 @@
-if (!config.依赖) {
-    //远程依赖索引文件代理地址列表
-    let requirelist = ['https://raw.githubusercontent.com/', 'https://ghproxy.com/https://raw.githubusercontent.com/', 'https://cdn.staticaly.com/gh/', 'https://github.jevons.vip/https://raw.githubusercontent.com/', 'https://cdn.jsdelivr.net/gh/Codebglh/hikerule/main/l/BGMH.js']
-    for (let i = 0; i < requirelist.length; i++) {
-        try {//远程依赖索引
-            if (i != requirelist.length - 1) {
-                require(requirelist[i] + 'Codebglh/hikerule/main/l/BGMH.js');
-                if (relyfile) {
-                    break;
-                }
-            } else {
-                require(requirelist[i])
+let requirelist = ["https://raw.githubusercontent.com/", "https://ghproxy.com/https://raw.githubusercontent.com/", "https://cdn.staticaly.com/gh/", "https://github.jevons.vip/https://raw.githubusercontent.com/"]
+for (let i = 0; i < requirelist.length; i++) {
+    try {//远程依赖索引
+        if (i != requirelist.length - 1) {
+            relyfile = require(requirelist[i] + 'Codebglh/hikerule/main/l/BGMH.js');
+            if (relyfile != null) {
+                break;
             }
-        } catch (e) {
-            log(e.message);
+        } else {
+            relyfile = require(requirelist[i])
         }
+    } catch (e) {
+        log(e.message);
     }
-    initConfig({
-        依赖: relyfile
-    });
 }
+initConfig({
+    依赖: relyfile
+});
+
