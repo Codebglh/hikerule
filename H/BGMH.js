21eebcc1412b35906729419367c4350c5be3f21f
bgcode <bglhcode@icloud.com>
2023-02-08 18:02:55 +0800

    bgcode

--- H/BGMH.js
+++ H/BGMH.js
@@ -72,14 +72,10 @@ function erji() {
     var GX = xpath(html, "//*[@id=\"book\"]/div[1]/p[3]/text()")
     var XQ = xpath(html, "//*[@id=\"book\"]/div[1]/p[4]/text()")
     d.push({
-        title: "‘‘’’<b>" + SM + '</b>' + "\n" + ' <small> \n作者：<font color="#FA7298">' + ZZ + '/n' +
-            '</font> \n类型：' + a1 + a2 +
-            "</small>",
+        title: SM + "作者：" + ZZ + "类型：" + a1 + a2,
         desc: '‘‘’’<font color="#f8ecc9">' + GX + "\n" + XQ + "</font>",
-
         pic_url: FM,
         col_type: 'movie_1_vertical_pic_blur',
-
     })
 
 
@@ -88,7 +84,6 @@ function erji() {
     for (var i = 0; i < BT.length; i++) {
         var a = LJ[i];
         let url = eval(a)
-        // log(url)
         d.push({
             title: BT[i], url: $(url).lazyRule(() => {
                 require(config.依赖);
@@ -121,27 +116,27 @@ function sanji(url) {
 
 }
 
-function search() {
-    var d = [];
-    var html = getResCode()
-    var url = 'https://www.mhww.xyz'
-    var BT = xpathArray(html, '//*[@id="booklist"]/div/div/div/p/span/text()');
-    var LJ = xpathArray(html, '//*[@id="booklist"]/div/div/@onclick');
-    var XQ = xpathArray(html, '//*[@id="booklist"]/div/div/div[2]/p[2]/text()');
-    var TP = xpathArray(html, '//*[@id="booklist"]/div/div/div[1]/img/@src');
-    for (var i = 0; i < BT.length; i++) {
-        var a = LJ[i];
-        var b = url + a.replace(/window\.open\(\'|\'\)/g, "")
-        d.push({
-            title: BT[i],
-            url: b + "#immersiveTheme#",
-            img: TP[i],
-            desc: XQ[i].replace("\r\n", ""),
-        });
-
-    }
-    setResult(d);
-}
+// function search() {
+//     var d = [];
+//     var html = getResCode()
+//     var url = 'https://www.mhww.xyz'
+//     var BT = xpathArray(html, '//*[@id="booklist"]/div/div/div/p/span/text()');
+//     var LJ = xpathArray(html, '//*[@id="booklist"]/div/div/@onclick');
+//     var XQ = xpathArray(html, '//*[@id="booklist"]/div/div/div[2]/p[2]/text()');
+//     var TP = xpathArray(html, '//*[@id="booklist"]/div/div/div[1]/img/@src');
+//     for (var i = 0; i < BT.length; i++) {
+//         var a = LJ[i];
+//         var b = url + a.replace(/window\.open\(\'|\'\)/g, "")
+//         d.push({
+//             title: BT[i],
+//             url: b + "#immersiveTheme#",
+//             img: TP[i],
+//             desc: XQ[i].replace("\r\n", ""),
+//         });
+//
+//     }
+//     setResult(d);
+// }
 
 function zhuye() {
     var d = [];
@@ -181,45 +176,36 @@ function sousuo() {
         pic_url: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=3779990328,1416553241&fm=179&app=35&f=PNG?w=60&h=70&s=E7951B62A4639D153293A4E90300401B',
         col_type: 'icon_small_3'
     });
-    var url = 'https://www.mhww.xyz'
-    // var MY_URL = " https://www.mhww.xyz/?page.currentPage=" + MY_PAGE + "&orderType=3&subjectName=%E9%9F%93%E6%BC%AB&filmName=" + input;
-    // var html = fetch(MY_URL);
-    // var BT = xpathArray(html, '//*[@id="booklist"]/div/div/div/p/span/text()');
-    // var LJ = xpathArray(html, '//*[@id="booklist"]/div/div/@onclick');
-    // var XQ = xpathArray(html, '//*[@id="booklist"]/div/div/div[2]/p[2]/text()');
-    // var TP = xpathArray(html, '//*[@id="booklist"]/div/div/div[1]/img/@src');
-    // for (var i = 0; i < BT.length; i++) {
-    //     var a = LJ[i];
-    //     var b = url + a.replace(/window\.open\(\'|\'\)/g, "")
-    //     d.push({
-    //         title: BT[i],
-    //         url: b + "#immersiveTheme#",
-    //         img: TP[i],
-    //         desc: XQ[i].replace("\r\n", ""),
-    //     });
-    //
-    // }
-    //
-
     setResult(d);
 };
 
 
-function a(x) {
-    eval(getCryptoJS());
-    const j = CryptoJS.enc.Utf8.parse('12cdefgabcdefg12');
-    let j1 = CryptoJS.enc.Utf8.parse(x);
-    let jg = CryptoJS.AES.encrypt(j1, j, {
-        'mode': CryptoJS.mode.ECB, 'padding': CryptoJS.pad.Pkcs7
-    });
-    return jg.toString();
-}
-
+// function a(x) {
+//     eval(getCryptoJS());
+//     const j = CryptoJS.enc.Utf8.parse('12cdefgabcdefg12');
+//     let j1 = CryptoJS.enc.Utf8.parse(x);
+//     let jg = CryptoJS.AES.encrypt(j1, j, {
+//         'mode': CryptoJS.mode.ECB, 'padding': CryptoJS.pad.Pkcs7
+//     });
+//     return jg.toString();
+// }
+//
+//
+// function openMH(bookid, linkid, path) {
+//     var localhost = 'https://www.mhdnf.xyz'
+//     // var idx = id.toString()
+//     let url = localhost + '/play?linkId=' + linkid + '&bookId=' + bookid + "&path=" + path + '&key=' + a(linkid)
+//     return url
+// }
 
 function openMH(bookid, linkid, path) {
     var localhost = 'https://www.mhdnf.xyz'
-    // var idx = id.toString()
-    let url = localhost + '/play?linkId=' + linkid + '&bookId=' + bookid + "&path=" + path + '&key=' + a(linkid)
+    eval(getCryptoJS());
+    const j = CryptoJS.enc.Utf8.parse('12cdefgabcdefg12');
+    let j1 = CryptoJS.enc.Utf8.parse(linkid);
+    let jg = CryptoJS.AES.encrypt(j1, j, {
+        'mode': CryptoJS.mode.ECB, 'padding': CryptoJS.pad.Pkcs7
+    });
+    let url = localhost + '/play?linkId=' + linkid + '&bookId=' + bookid + "&path=" + path + '&key=' + jg.toString()
     return url
 }
-
--- 学习/XQ.js
+++ 学习/XQ.js
@@ -181,3 +181,17 @@ function xq() {
     });
 
 }
+
+
+function openMH(bookid, linkid, path) {
+    var localhost = 'https://www.mhdnf.xyz'
+    eval(getCryptoJS());
+    const j = CryptoJS.enc.Utf8.parse('12cdefgabcdefg12');
+    let j1 = CryptoJS.enc.Utf8.parse(linkid);
+    let jg = CryptoJS.AES.encrypt(j1, j, {
+        'mode': CryptoJS.mode.ECB, 'padding': CryptoJS.pad.Pkcs7
+    });
+    let url = localhost + '/play?linkId=' + linkid + '&bookId=' + bookid + "&path=" + path + '&key=' + jg.toString()
+    return url
+}
+
