d9017f7ef8ce571463e91cc85786c0bf98d58f7f
bgcode <bglhcode@icloud.com>
2023-02-11 10:52:32 +0800

    bgcode 修改详情页

--- XQ/test.js
+++ XQ/test.js
@@ -75,48 +75,48 @@ var erjimenu = [
         }),
         col_type: 'scroll_button'
     },
-    // {
-    //     title: getMyVar('path') == '1' ? '““””<b><span style="color: #FF0000">线路三</span></b>' : '““””<b><span style="color: #1aad19">线路三</span></b>',
-    //     url: $("#noLoading#").lazyRule(() => {
-    //         if (getMyVar('path') == '1') {
-    //             putMyVar('path', '0');
-    //         } else {
-    //             putMyVar('path', '1');
-    //         }
-    //         ;
-    //         refreshPage(false);
-    //         return 'toast://切换路线成功'
-    //     }),
-    //     col_type: 'scroll_button'
-    // },
-    // {
-    //     title: getMyVar('path') == '1' ? '““””<b><span style="color: #FF0000">线路四</span></b>' : '““””<b><span style="color: #1aad19">线路四</span></b>',
-    //     url: $("#noLoading#").lazyRule(() => {
-    //         if (getMyVar('path') == '1') {
-    //             putMyVar('path', '0');
-    //         } else {
-    //             putMyVar('path', '1');
-    //         }
-    //         ;
-    //         refreshPage(false);
-    //         return 'toast://切换路线成功'
-    //     }),
-    //     col_type: 'scroll_button'
-    // },
-    // {
-    //     title: getMyVar('path') == '1' ? '““””<b><span style="color: #FF0000">线路五</span></b>' : '““””<b><span style="color: #1aad19">线路五</span></b>',
-    //     url: $("#noLoading#").lazyRule(() => {
-    //         if (getMyVar('path') == '1') {
-    //             putMyVar('path', '0');
-    //         } else {
-    //             putMyVar('path', '1');
-    //         }
-    //         ;
-    //         refreshPage(false);
-    //         return 'toast://切换路线成功'
-    //     }),
-    //     col_type: 'scroll_button'
-    // },
+    {
+        title: getMyVar('path') == '2' ? '““””<b><span style="color: #FF0000">线路三</span></b>' : '““””<b><span style="color: #1aad19">线路三</span></b>',
+        url: $("#noLoading#").lazyRule(() => {
+            if (getMyVar('path') == '2') {
+                putMyVar('path', '0');
+            } else {
+                putMyVar('path', '2');
+            }
+            ;
+            refreshPage(false);
+            return 'toast://切换路线成功'
+        }),
+        col_type: 'scroll_button'
+    },
+    {
+        title: getMyVar('path') == '3' ? '““””<b><span style="color: #FF0000">线路四</span></b>' : '““””<b><span style="color: #1aad19">线路四</span></b>',
+        url: $("#noLoading#").lazyRule(() => {
+            if (getMyVar('path') == '3') {
+                putMyVar('path', '0');
+            } else {
+                putMyVar('path', '3');
+            }
+            ;
+            refreshPage(false);
+            return 'toast://切换路线成功'
+        }),
+        col_type: 'scroll_button'
+    },
+    {
+        title: getMyVar('path') == '4' ? '““””<b><span style="color: #FF0000">线路五</span></b>' : '““””<b><span style="color: #1aad19">线路五</span></b>',
+        url: $("#noLoading#").lazyRule(() => {
+            if (getMyVar('path') == '4') {
+                putMyVar('path', '0');
+            } else {
+                putMyVar('path', '4');
+            }
+            ;
+            refreshPage(false);
+            return 'toast://切换路线成功'
+        }),
+        col_type: 'scroll_button'
+    },
     {
         col_type: 'line'
     }
@@ -201,6 +201,7 @@ function erji() {
 
     var BT = xpathArray(html, "//*[@class=\"cell-title\"]/text()");
     var LJ = xpathArray(html, xpat[getMyVar('path')]);
+    log(LJ[0]);
     if (getMyVar('shsort') == '0') {
         for (var i = 0; i < BT.length; i++) {
             var a = LJ[i];
