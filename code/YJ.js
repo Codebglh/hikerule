3d0947713f612f4a7efeb00cf26ff11eaca23368
bgcode <bglhcode@icloud.com>
2023-02-19 21:35:42 +0800

    bgcode 修改依赖

--- code/YJ.js
+++ code/YJ.js
@@ -2,7 +2,6 @@ let localhost = 'https://ghproxy.com/https://raw.githubusercontent.com/Codebglh/
 
 function yiji() {
     require(config.依赖);
-    xiazai();
     require(localhost + "yiji.js");
 }
 
@@ -10,48 +9,10 @@ function erji() {
     require(localhost + "erji.js")
 }
 
-function sanji(input) {
-    var url = eval(input)
-
-    function openMH(bookid, linkid, path) {
-        var localhost = 'https://www.mhdnf.xyz'
-        eval(getCryptoJS());
-        const j = CryptoJS.enc.Utf8.parse('12cdefgabcdefg12');
-        let j1 = CryptoJS.enc.Utf8.parse(linkid);
-        let jg = CryptoJS.AES.encrypt(j1, j, {
-            'mode': CryptoJS.mode.ECB, 'padding': CryptoJS.pad.Pkcs7
-        });
-        let url = localhost + '/play?linkId=' + linkid + '&bookId=' + bookid + "&path=" + path + '&key=' + jg.toString()
-        return url
-    }
-
-    var html = fetch(url)
-    var xx = xpathArray(html, "//*[@id=\"imgList\"]/img/@src");
-    var BT = xpathArray(html, "//*[@id=\"imgList\"]/img/@data-original");
-    var ll = "pics://"
-    for (var i = 0; i < 3; i++) {
-        ll = ll + xx[i] + '&&'
-    }
-    for (var i = 0; i < BT.length; i++) {
-
-        if (i == BT.length - 1) {
-            ll = ll + BT[i]
-        } else {
-            ll = ll + BT[i] + '&&'
-        }
-    }
-
-}
 
 function search() {
     require(localhost + "search.js")
 }
 
-
-function xiazai() {
-    let name = 'hiker://files/rules/bgHouse/js/yiji.js';
-    let file = 'https://ghproxy.com/https://raw.githubusercontent.com/Codebglh/hikerule/main/code/YJ.js'
-    downloadFile(file, name)
-}
 
 
