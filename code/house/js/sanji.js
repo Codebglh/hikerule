a10bf9fd6d46a90437eff6a466c00687b5f2302f
bgcode <bglhcode@icloud.com>
2023-02-19 20:57:40 +0800

    bgcode 修改依赖

--- code/house/js/sanji.js
+++ code/house/js/sanji.js
@@ -1,15 +1,1 @@
-var html = fetch(url)
-var xx = xpathArray(html, "//*[@id=\"imgList\"]/img/@src");
-var BT = xpathArray(html, "//*[@id=\"imgList\"]/img/@data-original");
-var ll = "pics://"
-for (var i = 0; i < 3; i++) {
-    ll = ll + xx[i] + '&&'
-}
-for (var i = 0; i < BT.length; i++) {
 
-    if (i == BT.length - 1) {
-        ll = ll + BT[i]
-    } else {
-        ll = ll + BT[i] + '&&'
-    }
-}
