6969a9a5728f43b42f0adfd0cd1e157ac702e589
bgcode <bglhcode@icloud.com>
2023-02-19 20:12:08 +0800

    bgcode 修改依赖

---
+++ code/YJ.js
@@ -0,0 +1,26 @@
+let localhost = 'https://ghproxy.com/https://raw.githubusercontent.com/Codebglh/hikerule/main/code/house/js/';
+
+function yiji() {
+    require(config.依赖);
+    xiazai();
+    require(localhost + "yiji.js");
+}
+
+function erji() {
+    require(localhost + "erji.js")
+}
+
+function sanji() {
+    require(localhost + "sanji.js")
+}
+
+function search() {
+    require(localhost + "search.js")
+}
+
+
+function xiazai() {
+    let name = 'hiker://files/rules/bgHouse/js/yiji.js';
+    let file = 'https://ghproxy.com/https://raw.githubusercontent.com/Codebglh/hikerule/main/code/YJ.js'
+    downloadFile(file, name)
+}
--- code/main.js
+++ code/main.js
@@ -1,13 +1,8 @@
-let file = 'https://ghproxy.com/https://raw.githubusercontent.com/Codebglh/hikerule/main/code/yiji.js';
-let name = 'hiker://files/rules/bgHouse/js/yiji.js';
+let file = 'https://ghproxy.com/https://raw.githubusercontent.com/Codebglh/hikerule/main/code/YJ.js';
+
 
 let relyfile = "";
-if (fileExist(name)) {
-    relyfile = name;
-} else {
-    downloadFile(file, name)
-    log('ssa')
-}
+
 if (relyfile == "") {
     relyfile = file;
 }
