bb9966c3b4f201287ed05b16ba1682385ab23db4
bgcode <bglhcode@icloud.com>
2023-02-20 14:06:58 +0800

    bgcode访问本地文件

--- code/house.js
+++ code/house.js
@@ -2,7 +2,6 @@ let localhost = 'https://ghproxy.com/https://raw.githubusercontent.com/Codebglh/
 let localfile = 'hiker://files/rules/bgHouse/js/'
 
 function yiji() {
-    xiazai();
     require(localfile + "yiji.js");
 }
 
@@ -16,20 +15,18 @@ function search() {
 }
 
 
-function xiazai() {
-    file = ['yiji.js', 'erji.js', 'sanji.js']
-    for (var i = 0; i < file.length; i++) {
-        if (fileExist(localfile + file[i])) {
-            if (md5(localfile + file[i]) != md5(localhost + file[i])) {
-                deleteFile(localfile + file[i])
-                downloadFile(localhost + file[i], localfile + file[i])
-                toast(file[i] + '更新成功')
-            } else {
-                toast(file[i] + '已是最新')
-            }
-        } else {
+file = ['yiji.js', 'erji.js', 'sanji.js']
+for (var i = 0; i < file.length; i++) {
+    if (fileExist(localfile + file[i])) {
+        if (md5(localfile + file[i]) != md5(localhost + file[i])) {
+            deleteFile(localfile + file[i])
             downloadFile(localhost + file[i], localfile + file[i])
-            toast(file[i] + '下载更新成功')
+            toast(file[i] + '更新成功')
+        } else {
+            toast(file[i] + '已是最新')
         }
+    } else {
+        downloadFile(localhost + file[i], localfile + file[i])
+        toast(file[i] + '下载更新成功')
     }
 }
