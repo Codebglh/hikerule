1d1d765d29aa4457bfd5a8c1d64503512cb82a34
bgcode <bglhcode@icloud.com>
2023-02-20 13:55:31 +0800

    bgcode访问本地文件

--- code/house.js
+++ code/house.js
@@ -17,27 +17,19 @@ function search() {
 
 
 function xiazai() {
-    file = localhost + 'yiji.js';
-    local = localfile + 'yiji.js';
-    file1 = localhost + 'erji.js';
-    local1 = localfile + 'erji.js';
-    file2 = localhost + 'sanji.js';
-    local2 = localfile + 'sanji.js';
-    if (md5(file) == md5(local) && md5(file1) == md5(local1) && md5(file2) == md5(local2)) {
-        toast("已经是最新")
-    } else {
-        if (fileExist(local)) {
-            deleteFile(local)
+    file = ['yiji.js', 'erji.js', 'sanji.js']
+    for (var i = 0; i < file.length; i++) {
+        if (fileExist(localfile + file[i])) {
+            if (md5(localfile + file[i] != md5(localhost + file[i]))) {
+                deleteFile(localfile + file[i])
+                downloadFile(localhost + file[i], localfile + file[i])
+                toast('更新成功')
+            } else {
+                toast('已是最新')
+            }
+        } else {
+            downloadFile(localhost + file[i], localfile + file[i])
+            toast('下载更新成功')
         }
-        if (fileExist(local1)) {
-            deleteFile(local1)
-        }
-        if (fileExist(local2)) {
-            deleteFile(local2)
-        }
-        downloadFile(file, local)
-        downloadFile(file1, local1)
-        downloadFile(file2, local2)
-        toast('更新成功')
     }
 }
